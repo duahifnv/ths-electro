827c399c51f86598fa751d72ff16d15b6f66e336
Nikita Fomin <defdXs@yandex.ru>
2025-04-05 01:00:38 +0300

    updates

--- frontend/src/components/pages/general/HomePage/HomePage.jsx
+++ frontend/src/components/pages/general/HomePage/HomePage.jsx
@@ -1,9 +1,10 @@
 import { PageBase } from "../../../../react-envelope/components/pages/base/PageBase/PageBase";
+import { Headline } from "../../../../react-envelope/components/ui/labels/Headline/Headline";
 
 export const HomePage = () => {
     return (
         <PageBase>
-            
+            <Headline>Доступные тарифы</Headline>
         </PageBase>
     );
 };
--- frontend/src/components/pages/general/TariffPage/TariffPage.jsx
+++ frontend/src/components/pages/general/TariffPage/TariffPage.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import VBoxPanel from "../../../../react-envelope/components/layouts/VBoxPanel/VBoxPanel";
 import { PageBase } from "../../../../react-envelope/components/pages/base/PageBase/PageBase";
+import { Headline } from "../../../../react-envelope/components/ui/labels/Headline/Headline";
+import css from './TariffPage.module.css';
 
 export const TariffPage = () => {
     return (
         <PageBase>
-            
+            <Headline>Исходные данные</Headline>
+            <p>Внесите Ваши данные, чтобы алгоритм мог расчитать рекомендуемые тарифные планы.</p>
+
+            <VBoxPanel className={css.content} gap={'10px'}>
+                
+            </VBoxPanel>
         </PageBase>
     );
 };
