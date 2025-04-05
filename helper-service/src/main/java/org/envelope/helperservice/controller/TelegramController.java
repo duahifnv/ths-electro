5de3dcc86ebc4f38c135951f9ee65e42bfe0658b
Максим Фоминцев <max135603@gmail.com>
2025-04-05 04:05:02 +0300

    validation

--- helper-service/src/main/java/org/envelope/helperservice/controller/TelegramController.java
+++ helper-service/src/main/java/org/envelope/helperservice/controller/TelegramController.java
@@ -1,5 +1,7 @@
 package org.envelope.helperservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.envelope.helperservice.entity.Helper;
 import org.envelope.helperservice.exception.IllegalClientException;
@@ -7,10 +9,8 @@ import org.envelope.helperservice.exception.ResourceNotFoundException;
 import org.envelope.helperservice.service.HelperService;
 import org.envelope.helperservice.service.TelegramService;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chat")
@@ -20,8 +20,15 @@ public class TelegramController {
     private final HelperService helperService;
     @Value("${telegram.secret-key}")
     private String secretKey;
+    @Operation(summary = "Получить текущий размер очереди пользователей")
     @PostMapping("/queue")
-    public Integer getQueueSize(@RequestParam String tgId, @RequestParam String secretKey) {
+    @ResponseStatus(HttpStatus.OK)
+    public Long getQueueSize(@RequestParam @Schema(description = "ID помощника в телеграмме",
+                                         example = "993439102")
+                                 String tgId,
+                             @RequestParam @Schema(description = "Секретный ключ доступа к сервису",
+                                     example = "g6s75rjhWc6cWxsYf7KSPdl0rO6Rc0RQ")
+                             String secretKey) {
         try {
             Helper helper = helperService.findByTgId(tgId);
             if (!secretKey.equals(this.secretKey)) {
