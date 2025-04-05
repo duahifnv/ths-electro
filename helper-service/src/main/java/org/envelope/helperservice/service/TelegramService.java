464a8ab5c520c91e1192624f627faaf8e07d901c
Максим Фоминцев <max135603@gmail.com>
2025-04-05 04:04:22 +0300

    removed mock

--- helper-service/src/main/java/org/envelope/helperservice/service/TelegramService.java
+++ helper-service/src/main/java/org/envelope/helperservice/service/TelegramService.java
@@ -1,10 +1,14 @@
 package org.envelope.helperservice.service;
 
+import lombok.RequiredArgsConstructor;
+import org.envelope.helperservice.repository.UserRequestRepository;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TelegramService {
-    public Integer getQueueSize() {
-        return 10;
+    private final UserRequestRepository userRequestRepository;
+    public Long getQueueSize() {
+        return userRequestRepository.count();
     }
 }
